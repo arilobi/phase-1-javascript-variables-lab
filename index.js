ff933a9f44f22720919396c66fd7f5848d6cc7fb
Marion Nabulobi <arinabulobi@gmail.com>
2024-09-25 12:25:41 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution in this file!
+let companyName = 'Scuber';
+
+{
+ const companyName = 'Scuber';
+}
+
+let mostProfitableNeighborhood = "Chelsea";
+
+// let mostProfitableNeighborhood = "";
+{ 
+ let companyCeo = "Susan Smith";
+}
+// const companyCeo = "Susan Smith";
+
+// let companyCeo = "";
